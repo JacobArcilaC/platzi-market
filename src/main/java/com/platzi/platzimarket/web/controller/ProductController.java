a1260c1f4b3dcbec6614ecd57854b72704aa4436
Jacob Arcila Cardenas <arcilacardenasjacob@gmail.com>
2021-06-25 16:35:00 -0500

    se creo el servicio y el controlador de productos

--- src/main/java/com/platzi/platzimarket/web/controller/ProductController.java
+++ src/main/java/com/platzi/platzimarket/web/controller/ProductController.java
@@ -1,2 +1,49 @@
-package com.platzi.platzimarket.web.controller;public class ProductController {
+package com.platzi.platzimarket.web.controller;
+
+import com.platzi.platzimarket.domain.dto.Product;
+import com.platzi.platzimarket.domain.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/all")
+    public List<Product> getAll(){
+        return productService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Product> getProduct(@PathVariable("id") int id){
+        return productService.getProduct(id);
+    }
+
+    @GetMapping("/category/{categoryId}")
+    public Optional<List<Product>> getByCategory(@PathVariable("categoryId") int categoryId){
+        return productService.getByCategory(categoryId);
+    }
+
+    @PostMapping("/save")
+    public Product save(@RequestBody Product product){
+        return productService.save(product);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public boolean delete(@PathVariable("id") int id){
+        return  productService.delete(id);
+    }
+
+
+
 }
