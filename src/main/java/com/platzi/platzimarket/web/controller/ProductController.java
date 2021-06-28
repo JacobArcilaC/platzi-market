b41d736c93ba54fca6385f9a894d6c57295aeab7
Jacob Arcila Cardenas <arcilacardenasjacob@gmail.com>
2021-06-28 09:52:39 -0500

    se añadio swagger 2 y su configuración

--- src/main/java/com/platzi/platzimarket/web/controller/ProductController.java
+++ src/main/java/com/platzi/platzimarket/web/controller/ProductController.java
@@ -2,6 +2,10 @@ package com.platzi.platzimarket.web.controller;
 
 import com.platzi.platzimarket.domain.Product;
 import com.platzi.platzimarket.domain.service.ProductService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,17 +25,24 @@ public class ProductController {
     }
 
     @GetMapping("/all")
+    @ApiOperation("Get all supermarket products")
+    @ApiResponse(code = 200, message = "OK")
     public ResponseEntity<List<Product>> getAll(){
         return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
+    @ApiOperation("Search a product given an ID")
+    @ApiResponses({
+        @ApiResponse(code = 200, message = "OK"),
+        @ApiResponse(code = 404, message = "Product not found")
+    })
     public ResponseEntity<Product> getProduct(@PathVariable("id") int id){
         return ResponseEntity.of(productService.getProduct(id));
     }
 
     @GetMapping("/category/{categoryId}")
-    public ResponseEntity<List<Product>> getByCategory(@PathVariable("categoryId") int categoryId){
+    public ResponseEntity<List<Product>> getByCategory(@ApiParam(value = "The id of the product", required = true, example = "7") @PathVariable("categoryId") int categoryId){
         return ResponseEntity.of(productService.getByCategory(categoryId));
     }
 
